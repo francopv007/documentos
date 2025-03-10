bc9b0ea5c8d5c345cbafb4a673bf29c83afb015c
franco007 <francoantony.pv1234@gmail.com>
2025-03-10 18:23:14 -0500

    new 10-03-25

--- archivos/data.js
+++ archivos/data.js
@@ -1,1 +1,16 @@
-alert("hola franco")
+function Ejecutar(valor1, valor2, dato){
+	let suma = Number(valor1) + Number(valor2);	
+	return suma;
+}
+
+function Changes(){
+	let dato = document.getElementById("dato").value;
+
+	let valor1 = document.getElementById("valor1").value;
+	let valor2 = document.getElementById("valor2").value;
+	
+	document.getElementById("change").textContent=Ejecutar(valor1,valor2,dato);
+
+ 	
+}
+
