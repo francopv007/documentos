b9e80b52f061f8d6386dbf73b644978a7122296f
franco007 <francoantony.pv1234@gmail.com>
2025-03-12 18:45:35 -0500

    change servidor 1 12/03/2025

--- archivos/data.js
+++ archivos/data.js
@@ -1,25 +1,21 @@
-function Ejecutar(valor1, valor2, dato,dato2){
 
-	if(dato2=='-'){
-		let resta = Number(valor1) - Number(valor2);	
-		return resta;
-	}
-	if(dato=='+'){
-		let suma = Number(valor1) + Number(valor2);	
-		return suma;
-	}
-}
-
-function Changes(){
-	let dato = document.getElementById("suma").value;
-	let dato2 = document.getElementById("resta").value;
-
+function Suma( ){
 	let valor1 = document.getElementById("valor1").value;
 	let valor2 = document.getElementById("valor2").value;
-	console.log(dato)
-	console.log(dato2)
-	document.getElementById("change").textContent=Ejecutar(valor1,valor2,dato,dato2);
 
- 	
+	let suma = Number(valor1) + Number(valor2);
+	console.log(suma);
+	document.getElementById("change").innerHTML=suma;
+}
+
+function Resta(){
+	let valor1 = document.getElementById("valor1").value;
+	let valor2 = document.getElementById("valor2").value;
+
+	let resta = Number(valor1) - Number(valor2);
+		console.log(resta);
+
+	document.getElementById("change").innerHTML=resta;
+
 }
 
