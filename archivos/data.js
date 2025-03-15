6d83b4a8b653f90be291e0b82732dcc1b1f8f283
franco007 <francoantony.pv1234@gmail.com>
2025-03-15 12:31:05 -0500

    cambio 15/03/2025

--- archivos/data.js
+++ archivos/data.js
@@ -1,21 +1,33 @@
-let valor1 = document.getElementById("numero1").value;
-let valor2 = document.getElementById("numero2").value;
-console.log("este valor",valor1)
 
-function Suma(valor1, valor2 ){
-	
+function Suma( ){
+	let valor1 = document.getElementById("numero1").value;
+	let valor2 = document.getElementById("numero2").value;
+
 	let suma = Number(valor1) + Number(valor2);
-	console.log(valor1);
-	console.log(suma);
-	return suma;
+	document.getElementById("change").innerHTML=suma;
+
 }
 
 function Resta(){
-	let valor1 = document.getElementById("valor1").value;
-	let valor2 = document.getElementById("valor2").value;
+	let valor1 = document.getElementById("numero1").value;
+	let valor2 = document.getElementById("numero2").value;
 
 	let resta = Number(valor1) - Number(valor2);
 	document.getElementById("change").innerHTML=resta;
 
 }
 
+	// let a = document.getElementById("numero1").value;
+	// let b = document.getElementById("numero2").value;
+	// 	console.log(a)
+let a = 10;
+let b = 20;
+
+function Multi(a,b){
+	
+	console.log(a)
+	let resta = Number(a) * Number(b);
+	console.log(resta);
+	return document.getElementById("change").innerHTML=resta;
+
+}
