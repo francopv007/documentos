ac6814d5624013c4837c82e8d89e4ba06b38e195
franco007 <francoantony.pv1234@gmail.com>
2025-03-24 18:12:13 -0500

    change serv1 21/03/2025

--- archivos/script1.js
+++ archivos/script1.js
@@ -1,16 +1,35 @@
-function callback(entries){
+// function callback(entries){
+// 	entries.forEach(entry=>{
+// 		console.log(entry)
+// 	});
+// }
+// const options={
+// 	root: null,
+// 	rootMargin:'0px',
+// 	threshold:0
+// }
+
+// const observer = new IntersectionObserver(callback,options);
+// const element = document.querySelector('.element');
+
+// observer.observe(element);
+
+const images=document.querySelectorAll('.hero_figure');
+function triggerAnimation(entries){
 	entries.forEach(entry=>{
-		console.log(entry)
+		const image = entry.target.querySelector("img");
+		// console.log(image);
+		image.classList.toggle('unset', entry.isIntersecting);
+
+
 	});
 }
-const options={
-	root: null,
-	rootMargin:'0px',
-	threshold:0
+const options = {
+	root : null,
+	rootMargin:"0px",
+	threshold:1,
 }
-
-const observer = new IntersectionObserver(callback,options);
-const element = document.querySelector('.element');
-
-observer.observe(element);
-
+const observer = new IntersectionObserver(triggerAnimation,options);
+images.forEach(image=>{
+	observer.observe(image);
+})
