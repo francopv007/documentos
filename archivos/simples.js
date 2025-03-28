a691645b235b87bfc7d201aea84f1fa14261d60d
franco <francoantony.pv123@gmail.com>
2025-03-27 23:45:34 -0500

    change serv 01 27/03/2025

--- archivos/simples.js
+++ archivos/simples.js
@@ -1,8 +1,32 @@
+ function EnviarDato(){
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let dni = document.getElementById("dni").value;
+  let news = document.getElementById("new");
+//   console.log(nombre,apellido,dni);
 
 
-const valor = document.querySelectorAll("li");
-valor.forEach(function (e,i){
-    valor[i]=`dato ${e}`;
-    
+  document.getElementById("mostrarNombre").innerHTML=nombre;
+  document.getElementById("mostrarApellido").innerHTML=apellido;
+
+  let datos = document.querySelectorAll("span");
+
+// datos.forEach(e => {
+
+//     console.log(e);
+// });
+
+
+datos.forEach(function(e,i){
+
+    datos[i] = `dato ${e} - ${i}`;
+    console.log(datos[i]);
+    // let li = document.createElement('li');
+    // li.textContent = data;
+    // news.appendChild(li);
+    // console.log(data);
+    news.innerHTML=datos[i];
 });
-console.log(valor);
+
+ }
+
